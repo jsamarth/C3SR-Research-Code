f5339cd8264580339bdf18e8bcd7416a6db23957
Samarth Jain <j.samarth@outlook.com>
2017-06-06 13:27:57 +0530

    Made the changes to the dataset

--- mayo/MayoExtract.py
+++ mayo/MayoExtract.py
@@ -3,34 +3,11 @@
 import urllib2
 import bs4
 import json
+import os.path
 
 
 # #################### Helper functions
 
-# # Function to remove tags from the string
-# def simpleText(string):
-# 	if string.find("<") != -1:
-# 		length = len(string)
-# 		openBrackets = []
-# 		closeBrackets = []
-# 		for i in range(0, length):
-# 			if(string[i] == "<"):
-# 				openBrackets.append(i)
-# 			if(string[i] == ">"):
-# 				closeBrackets.append(i)
-
-# 		firstOcc = string[openBrackets[0]:closeBrackets[0] + 1]
-# 		secondOcc = string[openBrackets[1]:closeBrackets[1] + 1]
-
-# 		string = string.replace(firstOcc, '')
-# 		string = string.replace(secondOcc, '')
-
-# 		return string
-# 	else:
-# 		return string
-
-
-# ################# Creating the soup #####################
 
 def removeCharactersFromString(string):
 	temp = string
@@ -39,7 +16,7 @@ def removeCharactersFromString(string):
 	temp = temp.replace("<ul>", "")
 	temp = temp.replace("</ul>", "")
 	temp = temp.replace("<li>", "")
-	temp = temp.replace("</li>", " ")
+	temp = temp.replace("</li>", ".")
 	temp = temp.replace("<p>", "")
 	temp = temp.replace("</p>", " ")
 	temp = temp.replace("<br>", "")
@@ -51,6 +28,32 @@ def removeCharactersFromString(string):
 	temp = temp.replace("</h2>", " ")
 	temp = temp.replace("<h4>", "")
 	temp = temp.replace("</h4>", " ")
+
+	temp = temp.replace("Description and Brand Names", "")
+	temp = temp.replace("Descriptions", "")
+
+	temp = temp.replace("Before Using", "")
+	temp = temp.replace("Allergies", "")
+	temp = temp.replace("Pediatric", "")
+	temp = temp.replace("Geriatric", "")
+	temp = temp.replace("Pregnancy", "")
+	temp = temp.replace("Breastfeeding", "")
+	temp = temp.replace("Drug Interactions", "")
+	temp = temp.replace("Other Interactions", "")
+	temp = temp.replace("Other Medical Problems", "")
+
+	temp = temp.replace("Proper Use", "")
+	temp = temp.replace("Dosing", "")
+	temp = temp.replace("Missed Dose", "")
+	temp = temp.replace("Storage", "")
+
+	temp = temp.replace("Precautions", "")
+
+	temp = temp.replace("Side Effects", "")
+	temp = temp.replace("More common", "")
+	temp = temp.replace("Rare", "")
+	temp = temp.replace("More common", "")
+	temp = temp.replace("Less common", "")
 
 	first = temp.find("<ol")
 	while first != -1:	
@@ -66,6 +69,8 @@ def url_to_soup(url):
     html = urllib2.urlopen(req).read() 
     return bs4.BeautifulSoup(html, "html.parser")
 
+
+# ================== Get all the links from the file ==================
 def getAllLinks():
 	file = open("urls.txt", "r")
 	urls = []
@@ -76,11 +81,14 @@ def getAllLinks():
 	file.close()
 	return urls
 
-def createFiles(links):
 
+# =================== Scrape all the data ==========================
+def createFiles(links):
+	i = 1
 	for link in links:
 
 		url = link
+		print url
 		soup = url_to_soup(url)
 
 		# Check if pages are there or not
@@ -118,13 +126,21 @@ def createFiles(links):
 		# =============== Descriptions Page ==================
 
 		adTag = soup.find(id="adsmobileBottom")
-		providedTag = soup.find("span", class_="provided").parent
+		# providedTag = soup.find("span", class_="provided").parent
 
 		descTag = soup.find_all("h3", text="Descriptions")[0]
 
 		# Title
 		title = soup.find("div", class_="headers v2 lg").contents[1].find("a").string
-		print title
+		title = title.replace("/", "-")
+		# print title
+
+		print "=== " + str(i) + " -> " + title + " ===\n"
+		i = i + 1
+		# checks if the file already exists. If it does then skip it
+		if os.path.exists("drug-info/" + title + ".json"):
+			print "(exists)\n"
+			continue
 
 		# Descriptions
 		descString = ""
@@ -134,8 +150,8 @@ def createFiles(links):
 					break
 				if sibling is adTag:
 					continue
-				if sibling is providedTag:
-					continue
+				# if sibling is providedTag:
+				# 	continue
 				descString += str(sibling)
 		# print removeCharactersFromString(descString)
 
@@ -169,120 +185,7 @@ def createFiles(links):
 				buString += str(child)
 			# print removeCharactersFromString(buString)
 
-		# tags = []
-		# allTag = soup.find_all("h3", text="Allergies ")
-		# if len(allTag) != 0:
-		# 	tags.append(allTag[0])
-		# pedTag = soup.find_all("h3", text="Pediatric ")
-		# if len(pedTag) != 0:
-		# 	tags.append(pedTag[0])
-		# gerTag = soup.find_all("h3", text="Geriatric ")
-		# if len(gerTag) != 0:
-		# 	tags.append(gerTag[0])
-		# pregTag = soup.find_all("h3", text="Pregnancy ")
-		# if len(pregTag) != 0:
-		# 	tags.append(pregTag[0])
-		# bfTag = soup.find_all("h3", text="Breastfeeding ")
-		# if len(bfTag) != 0:
-		# 	tags.append(bfTag[0])
-		# diTag = soup.find_all("h3", text="Drug Interactions ")
-		# if len(diTag) != 0:
-		# 	tags.append(diTag[0])
-		# oiTag = soup.find_all("h3", text="Other Interactions ")
-		# if len(oiTag) != 0:
-		# 	tags.append(oiTag[0])
-		# ompTag = soup.find_all("h3", text="Other Medical Problems ")
-		# if len(ompTag) != 0:
-		# 	tags.append(ompTag[0])
 		
-		# i = 0
-
-		# # Allergies
-		# allString = ""
-		# if len(allTag) != 0 and tags[i] is allTag[0]:
-		# 	for sibling in tags[i].next_siblings:
-		# 		if sibling is tags[i+1]:
-		# 			break
-		# 		if sibling is adTag:
-		# 			continue
-		# 		allString += str(sibling)
-		# 	i = i + 1
-		# # print removeCharactersFromString(allString)
-
-		# if len(pregTag)!=0 and tags[i] is pregTag[0]:
-		# 	i = i + 1
-
-		# # Pediatric
-		# pedString = ""
-		# if len(pedTag) != 0 and tags[i] is pedTag[0]:
-		# 	for sibling in tags[i].next_siblings:
-		# 		if sibling is tags[i+1]:
-		# 			break
-		# 		if sibling is adTag:
-		# 			continue
-		# 		pedString += str(sibling)
-		# 	i = i + 1
-		# # print removeCharactersFromString(pedString)
-
-		# if len(pregTag)!=0 and tags[i] is pregTag[0]:
-		# 	i = i + 1
-
-		# # Geriatric
-		# gerString = ""
-		# if len(gerTag) != 0 and tags[i] is gerTag[0]:
-		# 	for sibling in tags[i].next_siblings:
-		# 		if sibling is tags[i+1]:
-		# 				break
-		# 		if sibling is adTag:
-		# 			continue
-		# 		gerString += str(sibling)
-		# 	i = i + 1
-		# # print removeCharactersFromString(gerString)
-
-		# if len(pregTag)!=0 and tags[i] is pregTag[0]:
-		# 	i = i + 1
-		
-		# # Breastfeeding
-		# bfString = ""
-		# if len(bfTag) != 0 and tags[i] is bfTag[0]:
-		# 	for sibling in tags[i].next_siblings:
-		# 		if sibling is tags[i+1]:
-		# 			break
-		# 		if sibling is adTag:
-		# 			continue
-		# 		bfString += str(sibling)
-		# 	i = i + 1
-		# # print removeCharactersFromString(bfString)
-
-		# if len(pregTag)!=0 and tags[i] is pregTag[0]:
-		# 	i = i + 1
-		
-		# # Drug Interactions
-		# diString = ""
-		# if len(diTag) != 0 and tags[i] is diTag[0]:
-		# 	for sibling in tags[i].next_siblings:
-		# 		if sibling is tags[i+1]:
-		# 			break
-		# 		if sibling is adTag:
-		# 			continue
-		# 		diString += str(sibling)
-		# 	i = i + 1
-		# # print removeCharactersFromString(diString)
-
-		# if len(pregTag)!=0 and tags[i] is pregTag[0]:
-		# 	i = i + 1
-		
-		# # Other Interactions
-		# oiString = ""
-		# if len(oiTag) != 0 and tags[i] is oiTag[0]:
-		# 	for sibling in tags[i].next_siblings:
-		# 		if type(sibling) is bs4.element.Comment or sibling is tags[i+1]:
-		# 			break
-		# 		if sibling is adTag:
-		# 			continue
-		# 		oiString += str(sibling)
-		# 	i = i + 1
-		# # print removeCharactersFromString(oiString)
 
 
 
@@ -314,80 +217,6 @@ def createFiles(links):
 				puString += str(child)
 			# print removeCharactersFromString(puString)
 
-		# tags = []
-		# properTag = soup.find_all("h2", text="Proper Use")
-		# if len(properTag) != 0:
-		# 	tags.append(properTag[0])
-		# dosingTag = soup.find_all("h3", text="Dosing ")
-		# if len(dosingTag) != 0:
-		# 	tags.append(dosingTag[0])
-		# mDoseTag = soup.find_all("h3", text="Missed Dose ")
-		# if len(mDoseTag) != 0:
-		# 	tags.append(mDoseTag[0])
-		# storageTag = soup.find_all("h3", text="Storage ")
-		# if len(storageTag) != 0:
-		# 	tags.append(storageTag[0])
-		
-		
-		# i = 0
-
-		# # Proper Use
-		# properString = ""
-		# if len(properTag) != 0 and tags[i] is properTag[0]:
-		# 	for sibling in tags[i].next_siblings:
-		# 		if sibling is tags[i+1]:
-		# 			break
-		# 		if sibling is adTag:
-		# 			continue
-		# 		if sibling is providedTag:
-		# 			continue
-		# 		properString += str(sibling)
-		# 	i = i + 1
-		# # print removeCharactersFromString(properString)
-
-		# # Dosing
-		# dosingString = ""
-		# if len(dosingTag) != 0 and tags[i] is dosingTag[0]:
-		# 	for sibling in tags[i].next_siblings:
-		# 		if sibling is tags[i+1]:
-		# 			break
-		# 		if sibling is adTag:
-		# 			continue
-		# 		if sibling is providedTag:
-		# 			continue
-		# 		dosingString += str(sibling)
-		# 	i = i + 1
-		# # print removeCharactersFromString(dosingString)
-
-		# # Missed Dose
-		# mDoseString = ""
-		# if len(mDoseTag) != 0 and tags[i] is mDoseTag[0]:
-		# 	for sibling in tags[i].next_siblings:
-		# 		if sibling is tags[i+1]:
-		# 			break
-		# 		if sibling is adTag:
-		# 			continue
-		# 		if sibling is providedTag:
-		# 			continue
-		# 		mDoseString += str(sibling)
-		# 	i = i + 1
-		# # print removeCharactersFromString(mDoseString)
-
-		# # Storage
-		# storageString = ""
-		# if len(storageTag) != 0 and tags[i] is storageTag[0]:
-		# 	for sibling in tags[i].next_siblings:
-		# 		if type(sibling) is bs4.element.Comment:
-		# 			break
-		# 		if sibling is adTag:
-		# 			continue
-		# 		if sibling is providedTag:
-		# 			continue
-		# 		storageString += str(sibling)
-		# 	i = i + 1
-		# # print removeCharactersFromString(storageString)
-
-
 
 
 		# =============== Precautions Page ==================
@@ -418,24 +247,6 @@ def createFiles(links):
 				precString += str(child)
 			# print removeCharactersFromString(precString)
 
-# # 		adTag = soup.find(id="adsmobileBottom")
-# # 		providedTag = soup.find("span", class_="provided").parent
-
-# # 		precautionsTag = soup.find_all("h2", text="Precautions")[0]
-
-
-# # 		# Precautions
-# # 		precautionsString = ""
-# # 		if len(precautionsTag) != 0:
-# # 			for sibling in precautionsTag.next_siblings:
-# # 				if type(sibling) is bs4.element.Comment:
-# # 					break
-# # 				if sibling is adTag:
-# # 					continue
-# # 				if sibling is providedTag:
-# # 					continue
-# # 				precautionsString += str(sibling)
-# # 		# print removeCharactersFromString(precautionsString)
 
 
 		# =============== Side Effects page ==================
@@ -467,29 +278,16 @@ def createFiles(links):
 				sideString += str(child)
 			# print removeCharactersFromString(sideString)
 
-# # 		adTag = soup.find(id="adsmobileBottom")
-# # 		providedTag = soup.find("span", class_="provided").parent
+		jsonData = {}
+		jsonData['title'] = title
+		jsonData['descriptions'] = removeCharactersFromString(descString)
+		jsonData['before using'] = removeCharactersFromString(buString)
+		jsonData['proper use'] = removeCharactersFromString(puString)
+		jsonData['precautions'] = removeCharactersFromString(precString)
+		jsonData['side effects'] = removeCharactersFromString(sideString)
 
-# # 		commonTag = soup.find_all("h4", text="More common")[0]
-# # 		commonTag2 = soup.find_all("h4", text="More common")[1]
-# # 		rareTag = soup.find_all("h4", text="Rare")[0]
-
-
-# # 		# More Common
-# # 		commonString = ""
-# # 		if len(commonTag) != 0:
-# # 			for sibling in commonTag.next_siblings:
-# # 				if sibling is rareTag:
-# # 					break
-# # 				if sibling is adTag:
-# # 					continue
-# # 				if sibling is providedTag:
-# # 					continue
-# # 				commonString += str(sibling)
-# # 		# print removeCharactersFromString(commonString)
-
-
-	
-
+		fileToSaveData = open("drug-info/" + title + ".json", "w")
+		fileToSaveData.write(json.dumps(jsonData))
+		fileToSaveData.close()
 		
-createFiles(getAllLinks()[0:7])
+createFiles(list(set(getAllLinks())))
